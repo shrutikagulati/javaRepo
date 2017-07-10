6df15a8fd72c598c54e21a178cb5b82a57112268
Shrutika Gulati <shrutika.gulati@accoliteindia.com>
2017-07-10 22:55:13 +0530

    HelloWorldClass.java

---
+++ HelloWorldClass.java
@@ -0,0 +1,16 @@
+package HelloWorld;
+
+public class HelloWorldClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		for(int i=1;i<15;i+=2)
+		{
+			i++;
+			System.out.println(i);
+		}
+	}
+		
+
+}
